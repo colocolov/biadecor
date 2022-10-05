05efa10a10b7c025705b8f7c771137057fe4ad64
Stas Colocolov <colocolov.stas@gmail.com>
2022-10-06 01:03:40 +0300

    add page with fabric and modal window

---
+++ #src/js/scripts/modal.js
@@ -0,0 +1,30 @@
+const modal = document.querySelector(".modal");
+const modalBtn = document.querySelectorAll("[data-toggle=modal]");
+const modalCloseBtn = document.querySelector(".modal__close");
+
+modal.addEventListener("click", (e) => {
+  const isModal = e.target.closest(".modal__inner");
+  if (!isModal) {
+    removeActive();
+  }
+});
+
+modalBtn.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    modal.classList.add("_active");
+    document.body.classList.add("_lock");
+  });
+});
+
+modalCloseBtn.addEventListener("click", removeActive);
+
+function removeActive() {
+  modal.classList.remove("_active");
+  document.body.classList.remove("_lock");
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" || e.code === "Escape") {
+    removeActive();
+  }
+});

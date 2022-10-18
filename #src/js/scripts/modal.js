7fe445b67661c218ee5218a7e66ab35479c26309
colocolov <colocolov.stas@gmail.com>
2022-10-18 12:28:55 +0300

    upd content + add tooltip

--- #src/js/scripts/modal.js
+++ #src/js/scripts/modal.js
@@ -1,7 +1,13 @@
+// modal
 const modal = document.querySelector(".modal");
 const modalBtn = document.querySelectorAll("[data-toggle=modal]");
 const modalCloseBtn = document.querySelector(".modal__close");
+// tooltip
+const tooltipBtn = document.querySelectorAll(".box-btn");
+const tooltipContent = document.querySelectorAll(".box-content");
+const tooltipCloseBtn = document.querySelectorAll(".box-content__close");
 
+// modal
 function removeActive() {
   modal.classList.remove("_active");
   document.body.classList.remove("_lock");
@@ -28,5 +34,22 @@ if (modal) {
     if (e.key === "Escape" || e.code === "Escape") {
       removeActive();
     }
+  });
+}
+
+// tooltip
+if (tooltipBtn) {
+  tooltipBtn.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      let close = el.childNodes[1].childNodes[1];
+      let content = el.childNodes[1];
+      content.classList.toggle("active");
+
+      close.addEventListener("click", () => {
+        if (content.classList.contains("active")) {
+          content.classList.remove("_active");
+        }
+      });
+    });
   });
 }

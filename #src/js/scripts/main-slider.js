98462558272f262006e3b73b3b5ec4223cfaa69b
Stas Colocolov <colocolov.stas@gmail.com>
2025-09-25 19:22:01 +0300

    delete before-bg from woks and change dimens slide

--- #src/js/scripts/main-slider.js
+++ #src/js/scripts/main-slider.js
@@ -7,11 +7,16 @@ const headerSlider = new Swiper(".main-slider", {
     prevEl: ".main-slider__btn-prev",
     clickable: true,
   },
+  preloadImages: false,
+    lazy: {
+      loadOnTransitionStart: false,
+      loadPrevNext: false,
+  },
   //эффект перехода слайда (только если показ по 1-му слайду)
-  // effect: "fade",
-  // fadeEffect: {
-  //   crossFade: true,
-  // },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
   // переключение при клике на слайд
   slideToClickedSlide: true,
 });
